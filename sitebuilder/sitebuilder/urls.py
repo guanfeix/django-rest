5cb976bc4b039cbc59c1600e8cb59531fb98773b
guanfeix <guanfeixiang@qccr.com>
2018-12-05 15:30:46 +0800

    end-force-django

--- sitebuilder/sitebuilder/urls.py
+++ sitebuilder/sitebuilder/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, re_path
+from django.conf.urls.static import static
 from .views import *
 urlpatterns = [
     re_path(r'^(?P<slug>[\w./-]+)/$', page, name='page'),
     path('', page, name='homepage'),
-]
+]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
